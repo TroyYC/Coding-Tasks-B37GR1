23b3ce95310de40de5bb047c9af6ed46d231a96b
Mokhinur <mokhinurrasulova@gmail.com>
2024-11-12 22:26:01 +0100

    Mokhinur's task

---
+++ src/Mokhinur/week1_solutions.java
@@ -0,0 +1,12 @@
+package Mokhinur;
+
+public class week1_solutions {
+    /*
+    Please write a method that can identify given number is odd or even
+
+    EX:
+    identify(5)  >  "Odd"
+    identify(6)  >   "Even"
+
+         */
+}
