0406a6f1c481d9c97647bddd9532258dfc5d7871
salman <salmanuyghur3@gmail.com>
2024-11-13 09:25:25 +0300

    salman's task

--- src/salman_uyghur/Week1_Solutions.java
+++ src/salman_uyghur/Week1_Solutions.java
@@ -14,4 +14,28 @@ public class Week1_Solutions {
     identify(6)  >   "Even"
 
          */
+
+    public static void main(String[] args) {
+
+        int[] x = {25,9,16,63,70,23};
+        int[] y = {41,55,72,55,20,10};
+        System.out.println("this array has"+sumEvenAndOdd(x));
+        System.out.println(sumEvenAndOdd(y));
+
+    }
+
+    public static int sumEvenAndOdd(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers){
+            if (number % 2 == 0){
+                sum += number;
+                System.out.println("even = "+number);
+
+            }else {
+                sum = number;
+                System.out.println("odd = "+number);
+
+            }
+        }return sum;
+    }
 }
