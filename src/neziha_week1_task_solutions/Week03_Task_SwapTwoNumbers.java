000371e73ca94c26c16c842ed2c9fbdb467d3a70
neziha <nadaasenova.uk@gmail.com>
2024-12-01 15:14:13 +0000

    Week01 Task Solutions

---
+++ src/neziha_week1_task_solutions/Week03_Task_SwapTwoNumbers.java
@@ -0,0 +1,31 @@
+package neziha_week1_task_solutions;
+
+public class Week03_Task_SwapTwoNumbers {
+
+    public static void main(String[] args) {
+
+
+
+
+
+        //  Swap two variable values without using a third variable
+
+        int a = 5;
+        int b = 7;
+
+        System.out.println("a = " + a);//5
+        System.out.println("b = " + b);//7
+
+        a = a + b; // 12
+        b = a - b; // 5
+        a = a - b; // 7
+
+
+        System.out.println("a = " + a); // 7
+        System.out.println("b = " + b); // 5
+
+
+    }
+
+
+}
