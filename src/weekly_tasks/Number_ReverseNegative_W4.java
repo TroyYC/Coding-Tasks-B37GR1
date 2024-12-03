7019023ae876b4599d2bec835244395bfc39900b
TroyYC <troy@cydeo.com>
2024-12-03 15:22:22 -0500

    week 4 task added...

---
+++ src/weekly_tasks/Number_ReverseNegative_W4.java
@@ -0,0 +1,21 @@
+package weekly_tasks;
+
+public class Number_ReverseNegative_W4 {
+
+    /*
+    Write a return method that can reverse
+negative number and return it as int
+
+      Ex:
+      input => -456
+      output => -654
+
+      Neziha is volunteer
+     */
+
+
+
+
+
+
+}
