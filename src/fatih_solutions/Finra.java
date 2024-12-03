097c9964f9df21ea54198b8fea1a85dfa2292c9a
fatihelmaoglu <fatihelmaogluu@gmail.com>
2024-12-03 18:18:04 +0100

    Swapping two values

---
+++ src/fatih_solutions/Finra.java
@@ -0,0 +1,29 @@
+package fatih_solutions;
+
+public class Finra {
+
+    public static void main(String[] args) {
+
+        printNumber();
+    }
+
+
+
+    public static void printNumber(){
+
+        for (int i = 1; i <= 30; i++) {
+            //if (i % 3 == 0 && i % 5 == 0)
+            if (i % 15 == 0){
+                System.out.println("FINRA");
+            }else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            }else {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
