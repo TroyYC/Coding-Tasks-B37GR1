d60b425df8c7c3eea0b2dfc6f7f38eed49ef5578
Masi1989 <m.ehsaniyan89@gmail.com>
2024-12-10 10:58:40 -0800

    Task4

---
+++ src/Masoumeh/Number_ReverseNegative_W4.java
@@ -0,0 +1,30 @@
+package Masoumeh;
+
+public class Number_ReverseNegative_W4 {
+    public static void main(String[] args) {
+        int input = -456;
+        int output = (input);
+        if (input>=0){
+           return;
+        }
+        System.out.println("output : " + output);
+        System.out.println("input : " + input);
+
+        /*Write a return method that can reverse
+negative number and return it as int
+      Ex:
+      input => -456
+      output => -654
+     */
+
+
+    }
+}
+
+
+
+
+
+
+
+
