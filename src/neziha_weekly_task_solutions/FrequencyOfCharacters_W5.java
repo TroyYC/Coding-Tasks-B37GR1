52ca514a82147a58b4b625914cef9c79365e18d4
neziha <nadaasenova.uk@gmail.com>
2024-12-15 00:35:37 +0000

    Week_05 Task Solution

--- src/neziha_weekly_task_solutions/FrequencyOfCharacters_W5.java
+++ src/neziha_weekly_task_solutions/FrequencyOfCharacters_W5.java
@@ -5,7 +5,7 @@ import jdk.jfr.Frequency;
 public class FrequencyOfCharacters_W5 {
 
     /*
-    Write function that can find the frequency of characters
+    Write function that can find the frequency of characters.
 
             Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
 
