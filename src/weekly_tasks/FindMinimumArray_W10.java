5174a46ee972c4182d6b0c56fa88b85d4233a766
TroyYC <troy@cydeo.com>
2025-01-28 15:20:37 -0500

    week 9 solution and w10 task added...

---
+++ src/weekly_tasks/FindMinimumArray_W10.java
@@ -0,0 +1,11 @@
+package weekly_tasks;
+
+public class FindMinimumArray_W10 {
+
+     /*
+    Write a function that can find the minimum number from an int Array
+     */
+
+
+    //Fatih is volunteer
+}
