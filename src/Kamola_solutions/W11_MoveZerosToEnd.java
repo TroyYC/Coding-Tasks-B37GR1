2bbbe0e2d49caff49548b544a3b12ef2cd11c47f
kamola abulqosim <kamola.abulqosimova@gmail.com>
2025-02-11 09:32:08 +0100

    W11_MoveZerosToEnd solution added

---
+++ src/Kamola_solutions/W11_MoveZerosToEnd.java
@@ -0,0 +1,51 @@
+package Kamola_solutions;
+
+import java.util.Arrays;
+
+public class W11_MoveZerosToEnd {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 0, 5, 0, 6, 9};
+        moveZerosToEnd(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void moveZerosToEnd(int[] arr) {
+
+        int left = 0; // Pointer for non-zero elements
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] != 0) {
+                // Swap non-zero element with the left pointer
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++; // Move the left pointer
+            }
+        }
+    }
+}
+
+
+
+ /*
+        int index = 0; // Position to place non-zero elements
+
+        // Move non-zero elements forward
+        for (int num : arr) {
+            if (num != 0) {
+                arr[index++] = num;}
+        }
+
+        // Fill remaining positions with zeros
+        while (index < arr.length) {
+            arr[index++] = 0;}
+        }*/
+ /*
+    write a program that can move all the zeros to the end of an int array
+
+    input =  [ 1, 2, 0, 0, 5, 0, 6, 9]
+    output = [ 1, 2, 5, 6, 9, 0, 0, 0]
+
+    // Kamola is volunteer
+     */
